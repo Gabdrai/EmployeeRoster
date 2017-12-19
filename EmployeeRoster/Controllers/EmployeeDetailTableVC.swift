cd4724a707baffeefb647fc21862ba1d560bbf9f
Gabriel Drai <Gabrieldrai@hotmail.fr>
2017-12-19 14:59:28 +0200

    set date picker to display and save the date shown

--- EmployeeRoster/Controllers/EmployeeDetailTableVC.swift
+++ EmployeeRoster/Controllers/EmployeeDetailTableVC.swift
@@ -14,6 +14,8 @@ class EmployeeDetailTableVC: UITableViewController, UITextFieldDelegate {
         static let unwindToListIndentifier = "UnwindToListSegue"
     }
     
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var dobLabel: UILabel!
     @IBOutlet weak var employeeTypeLabel: UILabel!
@@ -22,9 +24,12 @@ class EmployeeDetailTableVC: UITableViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         updateView()
+//        updateDateViews()
+        
     }
+    
     
     func updateView() {
         if let employee = employee {
@@ -40,10 +45,80 @@ class EmployeeDetailTableVC: UITableViewController, UITextFieldDelegate {
             navigationItem.title = "New Employee"
         }
     }
+    
+    @IBAction func datePickerValueChanged()
+    {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .medium
+        
+        dobLabel.text = dateFormatter.string(from: datePicker.date)
 
+    }
+
+////    func updateDateViews() {
+//        let dateFormatter = DateFormatter()
+//
+//        dateFormatter.dateStyle = .medium
+//
+//        dobLabel.text = dateFormatter.string(from: datePicker.date)
+//    }
+    
+    
+    let datePickerIndexPath = IndexPath(row: 2, section: 0)
+    
+    var isEditingBirthday: Bool = false {
+        didSet {
+            datePicker.isHidden = !isEditingBirthday
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        switch(indexPath.section, indexPath.row) {
+            
+        case (datePickerIndexPath.section, datePickerIndexPath.row):
+            if isEditingBirthday {
+                return 172.0
+            } else {
+                return 0
+            }
+       
+            
+        default:
+            return 44.0
+        }
+    }
+ 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch (indexPath.section, indexPath.row) {
+            
+            
+        case (datePickerIndexPath.section, datePickerIndexPath.row - 1):
+            if isEditingBirthday {
+                isEditingBirthday = false
+            } else if isEditingBirthday {
+                isEditingBirthday = false
+                isEditingBirthday = true
+            }
+            else {
+                isEditingBirthday = true
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+    
+        default:
+            break
+
+        }
+    }
+
+   
     @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
         if let name = nameTextField.text {
-            employee = Employee(name: name, dateOfBirth: Date(), employeeType: .exempt)
+            employee = Employee(name: name, dateOfBirth: datePicker.date, employeeType: .exempt)
             performSegue(withIdentifier: PropertyKeys.unwindToListIndentifier, sender: self)
         }
     }
@@ -59,3 +134,5 @@ class EmployeeDetailTableVC: UITableViewController, UITextFieldDelegate {
         return false
     }
 }
+
+
